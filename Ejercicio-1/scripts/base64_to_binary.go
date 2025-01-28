7bce7c6247d0900f227bab873c719efcb3f4d8b0
Mark Albrand <mark.albrand5@gmail.com>
2025-01-28 08:19:43 -0600

    Update base64_to_binary.go

--- Ejercicio-1/scripts/base64_to_binary.go
+++ Ejercicio-1/scripts/base64_to_binary.go
@@ -5,8 +5,11 @@ import "strings"
 // Base64ToBinary Transforma un caracter en base64 a binario.
 func binary(n rune) string {
 	var result string
+
+	// Iterar por los 6 bits del caracter
 	for i := 5; i >= 0; i-- {
-		bit := n & (1 << uint(i))
+		bit := n & (1 << uint(i)) // Extraer el bit i
+
 		if bit > 0 {
 			result += "1"
 		} else {
@@ -16,17 +19,24 @@ func binary(n rune) string {
 	return result
 }
 
+// Base64ToBinary Transforma un texto en base64 a binario.
 func Base64ToBinary(s string) string {
 	var result string
+
 	for _, c := range s {
+		if c == '=' {
+			// El caracter '=' es un separador de bytes, por lo que se ignora
+			continue
+		}
+
 		if c >= 'A' && c <= 'Z' {
-			result += binary(c - 'A')
+			result += binary(c - 'A') // 0-25
 		}
 		if c >= 'a' && c <= 'z' {
-			result += binary(c - 'a')
+			result += binary(c - 'a') // 26-51
 		}
 		if c >= '0' && c <= '9' {
-			result += binary(c - '0')
+			result += binary(c - '0') // 52-61
 		}
 		if c == '+' {
 			result += binary(62)
@@ -38,26 +48,34 @@ func Base64ToBinary(s string) string {
 	return result
 }
 
+// BinaryToBase64 Transforma un texto binario a base64.
 func BinaryToBase64(s string) string {
 	s = strings.Replace(s, " ", "", -1) // Elimina los espacios en blanco (separadores de bytes)
 
 	var result string
+
+	// Iterar por bloques de 6 bits
 	for i := 0; i < len(s); i += 6 {
-		var n rune
+		var n rune // Número de 6 bits
+
+		// Construir el número de 6 bits
 		for j := 0; j < 6; j++ {
 			n <<= 1
+
+			// Verificar si el bit j está activo
 			if i+j < len(s) && s[i+j] == '1' {
-				n |= 1
+				n |= 1 // Encender el bit menos significativo
 			}
 		}
+
 		if n < 26 {
-			result += string(n + 'A')
+			result += string(n + 'A') // A-Z
 		}
 		if n >= 26 && n < 52 {
-			result += string(n - 26 + 'a')
+			result += string(n - 26 + 'a') // a-z
 		}
 		if n >= 52 && n < 62 {
-			result += string(n - 52 + '0')
+			result += string(n - 52 + '0') // 0-9
 		}
 		if n == 62 {
 			result += "+"
