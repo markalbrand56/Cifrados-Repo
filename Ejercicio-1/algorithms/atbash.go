6ad9e7f7d64b3bcbfaf7eacd8042ac1410d0b19d
Mark Albrand <mark.albrand5@gmail.com>
2025-01-27 18:36:47 -0600

    Update atbash.go

--- Ejercicio-1/algorithms/atbash.go
+++ Ejercicio-1/algorithms/atbash.go
@@ -1,11 +1,14 @@
 package algorithms
 
+// Atbash Cifra un texto utilizando el cifrado Atbash.
 func Atbash(s string) string {
 	var result string
 	for _, c := range s {
+		// Si el carácter es una letra minúscula, se le resta a 'z' y se suma 'a'
 		if c >= 'a' && c <= 'z' {
 			result += string('z' - c + 'a')
 		}
+		// Si el carácter es una letra mayúscula, se le resta a 'Z' y se suma 'A'
 		if c >= 'A' && c <= 'Z' {
 			result += string('Z' - c + 'A')
 		}
