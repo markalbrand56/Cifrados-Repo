a96e47508d8867986ffbbce68dbb00edec59b607
Mark Albrand <mark.albrand5@gmail.com>
2025-01-28 08:01:47 -0600

    Update ascii_to_binary.go

--- Ejercicio-1/scripts/ascii_to_binary.go
+++ Ejercicio-1/scripts/ascii_to_binary.go
@@ -10,6 +10,7 @@ import (
 func AsciiToBinary(text string) string {
 	var binaryResult string
 
+	// char es un rune, que es un alias para int32 lo que significa que se está tomando su valor ASCII.
 	for _, char := range text {
 		binaryResult += fmt.Sprintf("%08b ", char) // 08b indica que se quiere el número en binario con 8 bits.
 	}
@@ -19,17 +20,19 @@ func AsciiToBinary(text string) string {
 
 // BinaryToAscii Función que convierte un texto binario a texto.
 func BinaryToAscii(binary string) string {
+	binary = strings.ReplaceAll(binary, " ", "")
+
 	var asciiResult strings.Builder
 
-	// Divide la cadena binaria en bloques de 8 bits
-	bits := strings.Fields(binary)
+	// Verifica que la longitud de la cadena binaria sea múltiplo de 8
+	if len(binary)%8 != 0 {
+		fmt.Println("Error: la longitud de la cadena binaria no es múltiplo de 8")
+		return ""
+	}
 
-	for _, bit := range bits {
-		// Verifica que el bloque tenga exactamente 8 bits
-		if len(bit) != 8 {
-			fmt.Println("Error: el bloque no tiene 8 bits, recuerda separar el input con espacios")
-			return ""
-		}
+	// Itera sobre la cadena binaria en bloques de 8 bits
+	for i := 0; i < len(binary); i += 8 {
+		bit := binary[i : i+8]
 
 		// Convierte el bloque binario a un entero
 		value, err := strconv.ParseInt(bit, 2, 64)
